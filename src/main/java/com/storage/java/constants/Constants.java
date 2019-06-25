36e3bf90b7cfbacfcc33e497aa0038cdec27454c
alexpench <alex.pench@gmail.com>
2019-06-25 13:21:52 +0200

    refactorinh

--- src/main/java/com/storage/java/constants/Constants.java
+++ src/main/java/com/storage/java/constants/Constants.java
@@ -1,5 +1,5 @@
 package com.storage.java.constants;
 
-public final class Constants { // TODO check static? or not
+public final class Constants { // TODO check static
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
 }
